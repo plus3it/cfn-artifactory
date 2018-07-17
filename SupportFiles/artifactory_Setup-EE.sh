cefbcb4d42ea0e1c0106c4f08cb09f14173dc575
Thomas H Jones II <github@xanthia.com>
2018-07-17 15:15:39 -0400

    'Rough-sketch' of steps to automate

--- SupportFiles/artifactory_Setup-EE.sh
+++ SupportFiles/artifactory_Setup-EE.sh
@@ -18,38 +18,52 @@ AFCLHOME="${AFSAHOME}-cluster"
 SHARDS3=(${ARTIFACAORY_S3_SHARD_LOCS//:/ })
 
 
-#################################################################
-##
-## Manual steps (ARTIFACTORY_APP_HOME=/var/opt/jfrog/artifactory)
-##
-## 1) yum install -y jfrog-artifactory-pro
-## 2) Install Enterprise license (file)
-##    - s3 to /etc/cfn/files
-##    - copy to ${ARTIFACTORY_APP_HOME}/etc/artifactory.lic
-##    - fix perms/ownerships/labels
-## 3) create ${ARTIFACTORY_APP_HOME}/etc/db.properties file
-##    type=postgresql
-##    driver=org.postgresql.Driver
-##    url=jdbc:postgresql://${ARTIFACTORY_DB_HOSTFQDN}:5432/${ARTIFACTORY_DB_INSTANCE}
-##    username=${ARTIFACTORY_DB_ADMIN}
-##    password=${ARTIFACTORY_DB_PASSWD}
-## 4) Link PostGresql JDBC into Tomcat lib-dir:
-##    ln -s $( rpm -ql postgresql-jdbc | grep jdbc.jar ) ${ARTIFACTORY_APP_HOME}/tomcat/lib
-## 5) Create the ${ARTIFACTORY_APP_HOME}/etc/security/master.key file
-##    install -b -m 000644 -o artifactory -g artifactory <( openssl rand -hex 16 ) \
-##      ${ARTIFACTORY_APP_HOME}/etc/security/master.key file
-## 6) Create ha-node.properties file:
-##    install -b -m 000644 -o artifactory -g artifactory /dev/null /var/opt/jfrog/artifactory/etc/ha-node.properties
-##    (
-##     echo node.id=$( hostname -s )
-##     echo contexturl=http://$( ip addr show eth0 | awk '/ inet /{print $2}' | sed 's#/.*$##' )/artifactory
-##     echo membership.port=10001
-##     echo primary=$( awk -F = '/ARTIFACTORY_CLUSTER_MASTER/{print $2}' /etc/cfn/Artifactory.envs )
-##     echo artifactory.ha.data.dir=/var/opt/jfrog/artifactory-cluster/data
-##     echo artifactory.ha.backup.dir=/var/opt/jfrog/artifactory-cluster/backup
-##     echo hazelcast.interface=$( ip addr show eth0 | awk '/ inet /{print $2}' | sed 's#/.*$##' )
-##    )
-## 7) Create binarystore.xml file:
-##    install -b -m 000644 -o artifactory -g artifactory /dev/null /var/opt/jfrog/artifactory/etc/binarystore.xml
-## 8) Ensure systemd unit-file has Environment=START_TMO=120 in [service] block
-##
+##############################################
+## "Rough-sketch" of procedures to automate ##
+##############################################
+aws s3 cp s3://<TOOL_BUCKET>/SupportFiles/app-config.sh /etc/cfn/scripts/
+aws s3 cp s3://<TOOL_BUCKET>/SupportFiles/artifactory-EE_setup.sh /etc/cfn/scripts/
+
+aws s3 sync s3://<TOOL_BUCKET>/Licenses/ /etc/cfn/files/
+
+bash /etc/cfn/scripts/artifactory-EE_setup.sh 
+
+init 6
+
+yum install -y centos-release-scl\* jfrog-artifactory-pro
+
+yum install -y rh-postgresql96
+
+if [[ $( grep -q START_TMO /usr/lib/systemd/system/artifactory.service )$? -eq 0 ]]
+then
+   echo "Delayed starte-timeout already present"
+else
+   sed -i '/\[Service]/s/$/\nEnvironment=START_TMO=120/' /usr/lib/systemd/system/artifactory.service &&
+     systemctl daemon-reload
+fi
+
+
+install -b -m 000644 -o artifactory -g artifactory <( cat /etc/cfn/files/ArtifactoryEE_* ) /var/opt/jfrog/artifactory/etc/artifactory.lic
+
+install -b -m 000644 -o artifactory -g artifactory <( cat /var/opt/jfrog/artifactory/misc/db/postgresql.properties && awk -F= '/_DB_/{print $2}' /etc/cfn/Artifactory.envs ) /var/opt/jfrog/artifactory/etc/db.properties
+
+ln -s $( rpm -ql postgresql-jdbc | grep jdbc.jar ) /var/opt/jfrog/artifactory/tomcat/lib/
+
+install -d -m 0750 -o artifactory -g artifactory /var/opt/jfrog/artifactory/etc/security
+
+chcon --reference /var/opt/jfrog/artifactory/etc/ /var/opt/jfrog/artifactory/etc/security
+
+install -b -m 000640 <( awk -F= '/CLUSTER_KEY/{print $2}' /etc/cfn/Artifactory.envs ) -o artifactory -g artifactory /var/opt/jfrog/artifactory/etc/security/master.key
+
+install -b -m 000644 -o artifactory -g artifactory <( 
+  echo node.id=$(hostname -s)
+  echo context.url=http://$( ip addr show eth0 | awk '/ inet /{print $2}' | sed 's#/.*$#:8081/artifactory#' )
+  echo membership.port=10001
+  echo primary=true
+  echo artifactory.ha.data.dir=/var/opt/jfrog/artifactory-cluster/data
+  echo artifactory.ha.backup.dir=/var/opt/jfrog/artifactory-cluster/backup
+  echo hazelcast.interface=$( ip addr show eth0 | awk '/ inet /{print $2}' | sed 's#/.*$##' )
+ ) /var/opt/jfrog/artifactory/etc/ha-node.properties
+
+install -d -m 0750 -o artifactory -g artifactory /var/opt/jfrog/artifactory-cluster/{backup,data,cache}/
+
